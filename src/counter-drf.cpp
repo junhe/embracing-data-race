0190fbd86c274bd854716cefee39612c4274fcbc
Jun He <jhe@cs.wisc.edu>
2014-03-21 19:28:04 -0400

    able to do counter with data race

--- src/counter-drf.cpp
+++ src/counter-drf.cpp
@@ -1,23 +1,26 @@
 /* 
-   Example code copied from:
-https://computing.llnl.gov/tutorials/pthreads/
+    Example code copied from:
+    https://computing.llnl.gov/tutorials/pthreads/
 */
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define NUM_THREADS 4
+#define MAX_N_THREADS 128
+
+long long global_counter = 0;
+long long cnt_per_thread = 0; // will be initialized in main()
 
 void *BusyWork(void *t)
 {
-    int i;
+    long long i;
     long tid;
     double result=0.0;
     tid = (long)t;
     printf("Thread %ld starting...\n",tid);
-    for (i=0; i<1000000; i++)
+    for (i=0; i<cnt_per_thread; i++)
     {
-        result = result + sin(i) * tan(i);
+        global_counter++;
     }
     printf("Thread %ld done. Result = %e\n",tid, result);
     pthread_exit((void*) t);
@@ -25,17 +28,26 @@ void *BusyWork(void *t)
 
 int main (int argc, char *argv[])
 {
-    pthread_t thread[NUM_THREADS];
+    pthread_t thread[MAX_N_THREADS];
     pthread_attr_t attr;
     int rc;
+    int nthreads;
     long t;
     void *status;
+
+    if ( argc != 3 ) {
+        printf("Usage: %s nthreads cnt_per_thread \n", argv[0]);
+        exit(1);
+    } 
+
+    nthreads = atoi(argv[1]);
+    cnt_per_thread = atol(argv[2]);
 
     /* Initialize and set thread detached attribute */
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-    for(t=0; t<NUM_THREADS; t++) {
+    for(t=0; t<nthreads; t++) {
         printf("Main: creating thread %ld\n", t);
         rc = pthread_create(&thread[t], &attr, BusyWork, (void *)t); 
         if (rc) {
@@ -47,7 +59,7 @@ int main (int argc, char *argv[])
 
     /* Free attribute and wait for the other threads */
     pthread_attr_destroy(&attr);
-    for(t=0; t<NUM_THREADS; t++) {
+    for(t=0; t<nthreads; t++) {
         rc = pthread_join(thread[t], &status);
         if (rc) {
             printf("ERROR; return code from pthread_join() is %d\n", rc);
@@ -57,6 +69,6 @@ int main (int argc, char *argv[])
                "%ld having a status of %ld\n",t,(long)status);
     }
 
-    printf("Main: program completed. Exiting.\n");
+    printf("global counter: %lld", global_counter);
     pthread_exit(NULL);
 }
